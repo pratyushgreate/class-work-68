84c4955fe59726b2e314c19e3e8dbcacda360499
pratyushgreate <72809240+pratyushgreate@users.noreply.github.com>
2022-06-06 15:19:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View} from 'react-native';
+import Bottomtabnavigator from './components/Bottomtabnavigator';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Bottomtabnavigator/>
+    )
+  }
+}
